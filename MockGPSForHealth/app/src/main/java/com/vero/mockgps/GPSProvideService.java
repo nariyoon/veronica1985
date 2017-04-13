45d6dc1be5f9c5fc7dc809767e60caf8b9dd995c
nari.yoon <nari8682@gmail.com>
2017-04-13 16:35:30 +0900

    add listview

--- MockGPSForHealth/app/src/main/java/com/vero/mockgps/GPSProvideService.java
+++ MockGPSForHealth/app/src/main/java/com/vero/mockgps/GPSProvideService.java
@@ -1,9 +1,19 @@
 package com.vero.mockgps;
 
+import android.Manifest;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -18,6 +28,7 @@ public class GPSProvideService extends IntentService {
     private static final String ACTION_START = "com.vero.mockgps.action.START";
     private static final String ACTION_PUSH = "com.vero.mockgps.action.PUSH";
     private static final String ACTION_END = "com.vero.mockgps.action.END";
+    private static final String EXTRA_CVS_FILE_NAME = "com.vero.mockgps.extra.cvs_file_name";
     private static final String TAG = GPSProvideService.class.getSimpleName();
 
     private MockLocationProvider mockLocationProvider;
@@ -46,10 +57,11 @@ public class GPSProvideService extends IntentService {
         context.startService(intent);
     }
 
-    public static void startActionPush(Context context) {
+    public static void startActionPush(Context context, String cvsFileName) {
         Log.d(TAG, "startActionPush: ");
         Intent intent = new Intent(context, GPSProvideService.class);
         intent.setAction(ACTION_PUSH);
+        intent.putExtra(EXTRA_CVS_FILE_NAME, cvsFileName);
         context.startService(intent);
     }
 
@@ -60,7 +72,8 @@ public class GPSProvideService extends IntentService {
             if (ACTION_START.equals(action)) {
                 handleActionStart();
             } else if (ACTION_PUSH.equals(action)) {
-                handleActionPush();
+                String cvsFileName = intent.getStringExtra(EXTRA_CVS_FILE_NAME);
+                handleActionPush(cvsFileName);
             } else if (ACTION_END.equals(action)) {
                 handleActionEnd();
             }
@@ -69,32 +82,36 @@ public class GPSProvideService extends IntentService {
 
     private void handleActionStart() {
         Log.d(TAG, "handleActionStart: ");
-        if (!mockLocationProvider.register()) {
-            Toast.makeText(this, "no provider", Toast.LENGTH_SHORT).show();
-            stopSelf();
+        try {
+            if (!mockLocationProvider.register()) {
+                showToast("no provider");
+            }
+        } catch (SecurityException e) {
+            showToast("go to developer settings and add mock gps provider");
         }
+
     }
 
-    private void handleActionPush() {
-        Toast.makeText(this, "push start", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "handleActionPush: ");
-        pushMockLocation();
+    private void handleActionPush(String cvsFileName) {
+        showToast("push start");
+        Log.d(TAG, "handleActionPush: " + cvsFileName);
+        pushMockLocation(cvsFileName);
     }
 
     private void handleActionEnd() {
-        Toast.makeText(this, "push end", Toast.LENGTH_SHORT).show();
+        showToast("push end");
         Log.d(TAG, "handleActionEnd: ");
         mockLocationProvider.unregister();
     }
 
-    private void pushMockLocation() {
+    private void pushMockLocation(String cvsFileName) {
 
         Log.d(TAG, "pushMockLocation: ");
 
         try {
             List<String> data = new ArrayList<>();
 
-            InputStream is = getAssets().open("slowly_walk.csv");
+            InputStream is = getAssets().open(cvsFileName);
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
             String line;
@@ -112,5 +129,14 @@ public class GPSProvideService extends IntentService {
             Log.e("LocationTest", e.getMessage(), e);
             e.printStackTrace();
         }
+    }
+
+    private void showToast(final String msg) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- MockGPSForHealth/app/src/main/java/com/vero/mockgps/MainActivity.java
+++ MockGPSForHealth/app/src/main/java/com/vero/mockgps/MainActivity.java
@@ -1,10 +1,27 @@
 package com.vero.mockgps;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,17 +29,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("MainActivity", "no permission: ");
+            Toast.makeText(this, "no permission", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        GPSProvideService.startActionStart(this);
-        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ListView listView = (ListView) findViewById(R.id.location_listview);
+        final LocationAdapter adapter = new LocationAdapter();
+        adapter.add("slowly_walk.csv", "13min, 1km", R.drawable.slowly_walk);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                GPSProvideService.startActionPush(MainActivity.this);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                GPSProvideService.startActionPush(MainActivity.this, adapter.getFileName(position));
             }
         });
+
+        GPSProvideService.startActionStart(this);
     }
 
     @Override
@@ -30,5 +60,61 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         GPSProvideService.startActionEnd(this);
 
+    }
+
+    class LocationAdapter extends BaseAdapter {
+        ArrayList<String> cvs_title_list;
+        ArrayList<String> cvs_content_list;
+        ArrayList<Integer> map_img_list;
+
+        LocationAdapter() {
+            cvs_title_list = new ArrayList<>();
+            cvs_content_list = new ArrayList<>();
+            map_img_list = new ArrayList<>();
+        }
+
+        public void add(String cvsTitle, String cvsContent, int imgRes) {
+            cvs_title_list.add(cvsTitle);
+            cvs_content_list.add(cvsContent);
+            map_img_list.add(imgRes);
+        }
+
+        @Override
+        public int getCount() {
+            return cvs_title_list.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return getFileName(position);
+        }
+
+        public String getFileName(int position) {
+            return cvs_title_list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View currentView = convertView;
+            Context context = parent.getContext();
+            if (currentView == null) {
+                LayoutInflater inflater = (LayoutInflater) context
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                currentView = inflater.inflate(R.layout.list_item, parent, false);
+            }
+            ((TextView) currentView.findViewById(R.id.cvs_title))
+                    .setText(cvs_title_list.get(position));
+            ((TextView) currentView.findViewById(R.id.cvs_content))
+                    .setText(cvs_content_list.get(position));
+            ((ImageView) currentView.findViewById(R.id.map_img))
+                    .setImageResource(map_img_list.get(position));
+
+            return currentView;
+        }
     }
 }
